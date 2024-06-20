f6661426ad5f9d4bdb55e64d9257277123b65e53
machara027 <Macharashvilikaxa1234@gmail.com>
2024-06-20 21:28:32 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const unreadMessages = document.querySelectorAll(".unread");
+const unread = document.getElementById("notifes");
+const markAll = document.getElementById("mark_all");
+unread.innerHTML=unreadMessages.length
+
+console.log(unreadMessages)
+
+unreadMessages.forEach((message) =>{
+    message.addEventListener('click', () =>{
+        message.classList.remove('unread');
+        const newUnreadMessages = document.querySelectorAll('.unread');
+        unread.innerText = newUnreadMessages.length;
+    })
+})
+
+markAll.addEventListener('click', () =>{
+     unreadMessages.forEach((message) =>{
+        message.classList.remove('unread');
+     });
+     const newUnreadMessages = document.querySelectorAll('.unread');
+     unread.innerHTML = newUnreadMessages.length;
+    }
+)
